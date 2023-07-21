bbfc61979735c4d4f5a672e4295e53afe1415ed1
Kenny Hector <kennyhector24@gmail.com>
2023-07-21 17:18:22 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+});
+
+app.get("/featured", (req, res) => {
+    res.render("featured.ejs")
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs")
+});
+
+
+app.get("/", (req, res) => {
+    res.render("index.html")
+});
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
